458ba3c7c7a7b56d78918418d7ecf7892426109a
Musta Mohamed <mustafa.mohamed.wd@gmail.com>
2020-06-12 20:43:15 +0200

    improve ui and main todo actions

--- src/components/todo-item.vue
+++ src/components/todo-item.vue
@@ -1,23 +1,151 @@
 <template lang="html">
-  <v-card>
+  <v-card :class="{'todo-card': true, running: todo.isRunning, completed: todo.isCompleted}">
     <v-card-title primary-title>
+      <div class='d-flex justify-content-between w-100'>
+        <h3 :class="{'headline mb-0': true, completed: todo.isCompleted}">{{ todo.title }}</h3>
+        <div class='todo-actions'>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon
+                :color="todo.isCompleted ? 'blue': ''"
+                v-bind="attrs"
+                v-on="on"
+                @click="markAsCompletedTodo">
+                check
+              </v-icon>
+            </template>
+            <span>{{ !todo.isCompleted ? 'Mark as completed' : 'Mark as uncompleted' }}</span>
+          </v-tooltip>
+          <v-tooltip
+            top
+            v-if="!todo.isRunning && !todo.isCompleted"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon
+                color="#a4b0be"
+                v-bind="attrs"
+                v-on="on"
+                @click="markAsRunningTodo">
+                not_started
+              </v-icon>
+            </template>
+            <span>Start Task</span>
+          </v-tooltip>
+          <v-tooltip
+            top
+            v-if="todo.isRunning && !todo.isCompleted"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon
+                color="#a4b0be"
+                v-bind="attrs"
+                v-on="on"
+                @click="markAsNotRunningTodo">
+                pause_circle_outline
+              </v-icon>
+            </template>
+            <span>Pause Task</span>
+          </v-tooltip>
+          <v-tooltip top>
+            <template v-slot:activator="{ on, attrs }">
+              <v-icon
+                color="red"
+                v-bind="attrs"
+                v-on="on">
+                delete_forever
+              </v-icon>
+            </template>
+            <span>Delete</span>
+          </v-tooltip>
+        </div>
+      </div>
       <div>
-        <h3 class="headline mb-0">Title</h3>
+        <v-chip
+          class="ma-2"
+          color="#2ed573"
+          text-color="white"
+          small
+          label
+          outlined
+        > Status
+        </v-chip>
       </div>
     </v-card-title>
-    <v-btn>
-      Complete
-    </v-btn>
-    <v-btn color="error">
-      Delete
-    </v-btn>
   </v-card>
 </template>
 
 <script>
-export default {
-}
+  import { VTooltip } from 'vuetify/lib'
+  import { VChip } from 'vuetify/lib'
+
+  export default {
+    props: {
+      todo: {
+        type: Object,
+        default: () => {
+
+        },
+        /*title: { type: String, default: 'Title' },
+        id: { type: Number, default: 0 },
+        isCompleted: { type: Boolean, default: false },
+        isDeleted: { type: Boolean, default: false },
+        isRunning: { type: Boolean, default: false },*/
+      },
+    },
+    components: {
+      VTooltip,
+      VChip
+    },
+    methods: {
+      markAsCompletedTodo: function() {
+        const status = 'completed';
+        this.$emit('onTodoStatusChanged', status, this.todo.id);
+      },
+      markAsNotCompletedTodo: function() {
+        const status = 'not completed';
+        this.$emit('onTodoStatusChanged', status, this.todo.id);
+      },
+      markAsRunningTodo: function() {
+        const status = 'running';
+        this.$emit('onTodoStatusChanged', status, this.todo.id);
+      },
+      markAsNotRunningTodo: function() {
+        const status = 'paused';
+        this.$emit('onTodoStatusChanged', status, this.todo.id);
+      }
+    }
+  }
 </script>
 
-<style lang="css">
+<style lang="less">
+  @cardBorderColor: #00b894;
+  @cardRunningBorderColor: #b2bec3;
+  @cardCompletedBorderColor: #0984e3;
+  .todo-card {
+    border-left: 5px solid @cardBorderColor !important;
+
+    &.completed {
+      border-left: 5px solid @cardCompletedBorderColor !important;
+    }
+
+    &.running {
+      border-left: 5px solid @cardRunningBorderColor !important;
+    }
+
+    .todo-actions {
+      text-align: right;
+
+      i {
+        cursor: pointer;
+      }
+    }
+
+    .w-100 {
+      width: 100%;
+    }
+
+    .completed {
+      text-decoration: line-through;
+    }
+  }
 </style>
--- src/components/todo-list.vue
+++ src/components/todo-list.vue
@@ -1,18 +1,75 @@
 <template lang="html">
   <div>
-    <TodoItem class="mb-3" />
-    <TodoItem class="mb-3"/>
+    <TodoItem
+      class="mb-3"
+      v-for="todo in todos.filter(isRunning)"
+      :key="todo.id"
+      :todo="todo"
+      @onTodoStatusChanged="onTodoStatusChanged"
+    />
+    <TodoItem
+      class="mb-3"
+      v-for="todo in todos.filter(isNotCompleted)"
+      :key="todo.id"
+      :todo="todo"
+      @onTodoStatusChanged="onTodoStatusChanged"
+    />
+    <TodoItem
+      class="mb-3"
+      v-for="todo in todos.filter(isCompleted)"
+      :key="todo.id"
+      :todo="todo"
+      @onTodoStatusChanged="onTodoStatusChanged"
+    />
   </div>
 </template>
 
 <script>
-import TodoItem from '@/components/todo-item';
+  import TodoItem from '@/components/todo-item';
 
-export default {
-  components: {
-    TodoItem,
+  export default {
+    components: {
+      TodoItem,
+    },
+    data: function () {
+      return {
+        todos: [{
+          id: 1,
+          title: 'Todo Title #1',
+          isCompleted: true,
+          isDeleted: false,
+          isRunning: false,
+        }, {
+          id: 2,
+          title: 'Todo Title #2',
+          isCompleted: false,
+          isDeleted: false,
+          isRunning: false
+        }, {
+          id: 3,
+          title: 'Todo Title #3',
+          isCompleted: false,
+          isDeleted: false,
+          isRunning: true,
+        }]
+      };
+    },
+    methods: {
+      isCompleted: (todo)=> {
+        return todo.isCompleted && !todo.isRunning;
+      },
+      isRunning: (todo) => {
+        return !todo.isCompleted && todo.isRunning;
+      },
+      isNotCompleted: (todo) => {
+        return !todo.isCompleted && !todo.isRunning;
+      },
+      onTodoStatusChanged: (status, id) => {
+        alert(status + id);
+        const idx = this.todos.findIndex(f => f.id == id);
+      }
+    }
   }
-}
 </script>
 
 <style lang="css">
